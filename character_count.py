d7ecebde778845f5ea0781981f8d863fa7392e76
myanhtrannguyen <myanh260305@gmail.com>
2024-06-17 11:40:29 +0700

    Delete character_count.py

--- character_count.py
+++
@@ -1,8 +0,0 @@
-def character_count (word):
-    character_statistic = {}
-    for char in word:
-        if char in character_statistic:
-            character_statistic[char] += 1
-        else:
-            character_statistic[char] = 1
-    return character_statistic
