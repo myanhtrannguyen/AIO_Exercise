7f53bc2d5333534e96d1aaba2939717896d576de
myanhtrannguyen <myanh260305@gmail.com>
2024-06-17 12:03:16 +0700

    Delete Mean Difference of nth Root Error.py

--- Mean Difference of nth Root Error.py
+++
@@ -1,8 +0,0 @@
-def MD_nRE(y, y_hat, n, p):
-    return((y ** (1 / n)) - (y_hat ** (1/n))) ** p
-if __name__ == "__main__":
-    y = float(input())
-    y_hat = float(input())
-    n = float(input())
-    p = float(input())
-    print(MD_nRE(y, y_hat, n, p))
